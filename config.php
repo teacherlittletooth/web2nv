003afcbc9f911697e5ecc0dc4277506cd043e50b
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2022-05-16 19:54:24 -0300

    Configurações do Banco de dados

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'boteco');
+define('MYSQL_USER',        'root');
+define('MYSQL_PASS',        '');
+define('MYSQL_PORT',        '3307');
+define('MYSQL_CHARSET',     'utf8');
